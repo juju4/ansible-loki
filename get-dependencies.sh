df93b9216c80ac84e36e587ecbc38d7a2c71997c
juju4 <juju4@users.noreply.github.com>
2020-02-16 14:10:18 -0500

    rhel/centos7: +python36

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,7 +10,7 @@ rolesdir=$(dirname $0)/..
 fi
 [ $APPVEYOR ] && rolesdir=/etc/ansible/roles
 
-#[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.loki ] && ln -s ansible-loki $rolesdir/juju4.loki
 [ ! -e $rolesdir/juju4.loki ] && cp -R $rolesdir/ansible-loki $rolesdir/juju4.loki
